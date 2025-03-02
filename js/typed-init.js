5f1ea9bc13462b9dc78c87adcf6b232577839ffc
Aaron McLean <aaronmclean26@gmail.com>
2025-03-02 03:25:50 -0500

    js3

--- js/typed-init.js
+++ js/typed-init.js
@@ -1,16 +1,17 @@
 var typed = new Typed('.auto-type', {
-    strings: [,
+    strings: [
       "mobile app architecture",
       "iOS development",
       "React.js application",
+      "quantum implementation",
       "neural network",
       "blockchain solution",
       "API integration",
-      "database",
-      "full-stack deployment",
-      "embedded systems",
+      "full-stack development",
+      "embedded system",
       "algorithm design",
-      "system optimization"
+      "system optimization",
+      "distributed system",
     ],
     typeSpeed: 80,
     backSpeed: 40,
