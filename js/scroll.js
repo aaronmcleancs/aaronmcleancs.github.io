77812cd688da6afb32fa83c56ffa5cbe8c773e7e
Aaron McLean <aaronmclean26@gmail.com>
2025-03-01 23:53:34 -0500

    js

--- js/scroll.js
+++ js/scroll.js
@@ -1,26 +1,25 @@
-$(window).scroll(function() {
-  if ($(this).scrollTop() > window.innerHeight - 300) {
-    $('#scroll').addClass('animate__fadeInUp');
-    $('#scroll').removeClass('animate__fadeOutDown');
-  }
+$(document).ready(function() {
+  $(window).on('scroll', function() {
+    var scrollTop = $(window).scrollTop();
+    var viewportHeight = $(window).height();
+
+    
+    if (scrollTop > viewportHeight - 300) {
+      $('#scroll').addClass('animate__fadeInUp').removeClass('animate__fadeOutDown');
+    }
+
+    
+    if (scrollTop > viewportHeight - 200) {
+      $('#card1').addClass('animate__fadeInUp visible');
+      setTimeout(function(){
+        $('#card2').addClass('animate__fadeInUp visible');
+      }, 50);
+      setTimeout(function(){
+        $('#card3').addClass('animate__fadeInUp visible');
+      }, 100);
+      setTimeout(function(){
+        $('#card4').addClass('animate__fadeInUp visible');
+      }, 150);
+    }
+  });
 });
-
-
-$(window).scroll(function() {
-  if ($(this).scrollTop() > window.innerHeight - 200) {
-    $('#card1').addClass('animate__fadeInUp visible');
-    setTimeout(function(){
-      $('#card2').addClass('animate__fadeInUp visible');
-    }, 50);
-    setTimeout(function(){
-      $('#card3').addClass('animate__fadeInUp visible');
-    }, 100);
-    setTimeout(function(){
-      $('#card4').addClass('animate__fadeInUp visible');
-    }, 150);
-  }
-});
-
-
-
-
